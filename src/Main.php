e5ef5e852fca73d1adf19cda5c494e03d297c43a
ohitszenqi <88894112+ohitszenqi@users.noreply.github.com>
2021-10-01 20:17:11 +0800

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -1,5 +1,6 @@
 <?php
 namespace EconomyAddon;
+# Imports the class
 use pocketmine\plugin\PluginBase;
 use Ifera\ScoreHud\scoreboard\ScoreTag;
 use Ifera\ScoreHud\event\PlayerTagUpdateEvent;
@@ -7,8 +8,14 @@ use Ifera\ScoreHud\event\ServerTagUpdateEvent;
 use Ifera\ScoreHud\event\TagsResolveEvent;
 use pocketmine\event\Listener;
 use onebone\economyapi\EconomyAPI;
-
+# Uses the main class and gives the permission to use events.
 class Main extends PluginBase implements Listener {
+  # register the events idk if its needed LOL
+  public function onEnabled() {
+    $this->getLogger()->info("Addon enabled.");
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+}
+# registers the economy tag :)
   public function onTagResolve(TagsResolveEvent $event){
       	$player = $event->getPlayer();
       	$tag = $event->getTag();
